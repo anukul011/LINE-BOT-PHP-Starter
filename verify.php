85f68c8ed947a767037acd9c497e6c7594589731
anukul011 <anukul@innova.or.th>
2017-09-25 15:52:29 +0700

    Create verify.php

---
+++ verify.php
@@ -0,0 +1,15 @@
+<?php
+$access_token = 'HQxTtGqXZheqlKDrBggH4CTFvtJsiYH9Z9KG1CFqrFh1VRrfDVcwJgnbaU08Vt6uhMf0qMAWsyCY7AeyjDfCtrspzVY3wMuCMnQ9eHNh7BNS4DWrFqZhmnJguhw/zA1sQkrWn4ZXZQHZtcMwiSLmZwdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
